2550305ec14462145c0f698a0a5739c9726f5442
mohsinghani <mohsinghani.777@gmail.com>
2017-10-18 07:53:28 -0700

    implement reserved slot logic

--- functions/src/lib/conversationFunction.ts
+++ functions/src/lib/conversationFunction.ts
@@ -90,7 +90,7 @@ export const apiAiResponse = functions.https.onRequest((requestt, response) => {
             break;
 
         case 'slot-number':
-            slotBook(requestt, response);
+            bookSlot(requestt, response);
             break;
 
         default:
@@ -119,7 +119,7 @@ function bookingSlotDate(request, response) {
         let year = new Date().getFullYear()
         let currentDate = `${year}-${month + 1}-${day}`
         let isDateValid = moment(date).isSameOrAfter(currentDate) && moment(date, "YYYY MM DD").isValid() ? true : false;
-        // console.log('115', isDateValid, currentDate, date)
+        console.log('122', isDateValid, currentDate, date)
         if (!isDateValid) {
             response.send(
                 {
@@ -208,23 +208,60 @@ function bookingEndTime(request, response) {
             }
             else {
                 if (date != undefined) {
-                    // checkReservedSlots(date, startTime, endTime,response)
-                    let reservedSlots = checkReservedSlots(date, startTime, endTime)
-                    let uniqueReservedSlots = reservedSlots.filter(function (elem, index, self) {
-                        return index == self.indexOf(elem);
-                    })
-                    response.send(
-                        {
-                            // 'speech': `You can Reserve one slot between 1 to 100 except these ${JSON.stringify(uniqueReservedSlots)}`
-                            'speech': `You can Reserve one slot between 1 to 100 except these ${JSON.stringify(uniqueReservedSlots)}`,
-                            "followupEvent": {
-                                "name": "slot-number",
-                                "data": {
-                                    "reserved-slots": JSON.stringify(uniqueReservedSlots)
+                    console.log('211', date, startTime, endTime, sessionId)
+                    let reservedSlots = [];
+                    checkReservedSlots(date, startTime, endTime, sessionId).then((snap) => {
+                        console.log('214: slots', snap)
+                        let slots = []
+                        let data = snap.val()
+                        for (var key in data) {
+                            let date = '2017-10-18'
+                            let startTime = '9';
+                            let endTime = '11'
+                            if (moment(date).isSame(data[key].date)) {
+                                let currentReservedHours = []
+                                let reservedHours = []
+                                for (let i = parseInt(startTime); i <= parseInt(endTime); i++) {
+                                    currentReservedHours.push(i)
                                 }
+                                for (let i = data[key].startTime; i <= data[key].endTime; i++) {
+                                    reservedHours.push(i)
+                                }
+                                let temp;
+                                for (let index = 0; index < reservedHours.length; index++) {
+                                    for (let index1 = 0; index1 < currentReservedHours.length; index1++) {
+                                        if (reservedHours[index] === currentReservedHours[index1]) {
+                                            if (temp != data[key].slotNumber) {
+                                                db.ref(`/helper/${sessionId}/reservedHours`).push(data[key].slotNumber)
+                                                slots.push(data[key].slotNumber)
+                                            }
+                                            temp = data[key].slotNumber
+                                        }
+                                    }
+                                }
+                                currentReservedHours = []
+                                reservedHours = []
                             }
                         }
-                    )
+                        return slots
+                    }).then(() => {
+                        db.ref(`/helper/${sessionId}/reservedHours`).on('child_added', (snapshot) => {
+                            reservedSlots.push(snapshot.val())
+                        })
+                        setTimeout(()=>{
+                            response.send(
+                                {
+                                    'speech': `You can Reserve one slot between 1 to 100 except these ${JSON.stringify(reservedSlots)}`,
+                                    "followupEvent": {
+                                        "name": "slot-number",
+                                        "data": {
+                                            "reserved-slots": JSON.stringify(reservedSlots)
+                                        }
+                                    }
+                                }
+                            )
+                        },3000)  
+                    })
                 }
                 else {
                     response.send(
@@ -238,40 +275,81 @@ function bookingEndTime(request, response) {
     })
 }
 
-
-
-function checkReservedSlots(date, startTime, endTime) {
+function checkReservedSlots(date, startTime, endTime, sessionId) {
+    return db.ref('/CDGKreservedSlotsList/').once('value', (snapshot) => {
+        return snapshot.val()
+    })
     // console.log('date, start time, end time', date, startTime, endTime)
-    let reservedSlots: any = []
+    // let reservedSlots: any = []
     // getting reserved slots from database
-    db.ref('/CDGKreservedSlotsList/').on('child_added', (snapshot) => {
-        // console.log('parking data', snapshot.val())
-        // console.log('date->',date)
-        // console.log('db date->',snapshot.val().date)
-        if (moment(date).isSame(snapshot.val().date)) {
-            // console.log('date are matche->slot number-> ',snapshot.val().slotNumber)
-            let currentReservedHours = []
-            let reservedHours = []
-            for (let i = parseInt(startTime); i <= parseInt(endTime); i++) {
-                currentReservedHours.push(i)
-            }
-            for (let i = snapshot.val().startTime; i <= snapshot.val().endTime; i++) {
-                reservedHours.push(i)
-            }
-            // console.log('reseved hours', reservedHours)
-            // console.log('currentReservedHours', currentReservedHours)
-            for (let index = 0; index < reservedHours.length; index++) {
-                for (let index1 = 0; index1 < currentReservedHours.length; index1++) {
-                    if (reservedHours[index] === currentReservedHours[index1]) {
-                        reservedSlots.push(snapshot.val().slotNumber)
-                    }
+    // let slots: any = []
+    // db.ref('/CDGKreservedSlotsList/').on('child_added', (snapshot) => {
+    //     // console.log('parking data', snapshot.val())
+    //     // console.log('date->',date)
+    //     // console.log('db date->',snapshot.val().date)
+    //     slots = new Promise((res, rej) => {
+    //         if (moment(date).isSame(snapshot.val().date)) {
+    //             // console.log('date are matche->slot number-> ',snapshot.val().slotNumber)
+    //             let currentReservedHours = []
+    //             let reservedHours = []
+    //             for (let i = parseInt(startTime); i <= parseInt(endTime); i++) {
+    //                 currentReservedHours.push(i)
+    //             }
+    //             for (let i = snapshot.val().startTime; i <= snapshot.val().endTime; i++) {
+    //                 reservedHours.push(i)
+    //             }
+    //             // console.log('reseved hours', reservedHours)
+    //             // console.log('currentReservedHours', currentReservedHours)
+    //             let temp;
+    //             for (let index = 0; index < reservedHours.length; index++) {
+    //                 for (let index1 = 0; index1 < currentReservedHours.length; index1++) {
+    //                     if (reservedHours[index] === currentReservedHours[index1]) {
+    //                         // reservedSlots.push(snapshot.val().slotNumber)
+    //                         if (temp != snapshot.val().slotNumber) {
+    //                             db.ref(`/helper/${sessionId}/reservedHours`).push(snapshot.val().slotNumber).then(() => {
+    //                                 res('successfull')
+    //                             }).catch(() => {
+    //                                 rej()
+    //                             })
+    //                         }
+    //                         temp = snapshot.val().slotNumber
+    //                     }
+    //                 }
+    //             }
+    //             currentReservedHours = []
+    //             reservedHours = []
+    //         }
+    //     })
+    // })
+    // return slots
+    // console.log('reservedSlots in function', reservedSlots)
+    // return reservedSlots;
+}
+
+function bookSlot(request, response) {
+    const uSlotNumber = request.body.result.parameters.slotNumber;
+    const sessionId = request.body.sessionId;
+
+    let temp = true;
+    db.ref(`/helper/${sessionId}/reservedHours`).on('child_added', (snapshot) => {
+        console.log('291:reserved->', snapshot.val())
+        if (parseInt(snapshot.val()) == parseInt(uSlotNumber)) {
+            console.log("292:reserved after if->", parseInt(snapshot.val()) + " == " + parseInt(uSlotNumber))
+            db.ref(`/helper/${sessionId}`).remove()
+            response.send(
+                {
+                    'speech': `You cannot reserve this slot`
                 }
-            }
-            currentReservedHours = []
-            reservedHours = []
+            )
+            temp = false;
         }
     })
-    // console.log('reservedSlots in function', reservedSlots)
-    return reservedSlots;
-
+    if (temp) {
+        db.ref(`/helper/${sessionId}`).remove()
+        response.send(
+            {
+                'speech': `congratulation you have reserved slot number ${uSlotNumber}`
+            }
+        )
+    }
 }
